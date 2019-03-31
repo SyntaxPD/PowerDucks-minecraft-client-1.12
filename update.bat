7a9069fd87e681094fc950b027bbb522381b2cd5
SyntaxPD <fromeast@rambler.ru>
2019-03-31 18:34:46 +0300

    updater

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 
-start /wait /b "" "git\git-cmd.exe" git remote add origin https://github.com/SyntaxPD/PowerDucks-minecraft-client.git
+start /wait /b "" "git\git-cmd.exe" git remote add origin https://github.com/SyntaxPD/PowerDucks-minecraft-client-1.12
 timeout /t 3 /nobreak >nul
 
 start /wait /b "" "git\git-cmd.exe" git remote -v
