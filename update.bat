a254f1a1466a1af8e7fc00d7d3efadd6ac087f4f
SyntaxPD <fromeast@rambler.ru>
2019-03-31 18:22:51 +0300

    Initial commit

---
+++ update.bat
@@ -0,0 +1,14 @@
+echo off
+cls
+
+start /wait /b "" "git\git-cmd.exe" git remote add origin https://github.com/SyntaxPD/PowerDucks-minecraft-client.git
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git remote -v
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git pull
+timeout /t 3 /nobreak >nul
+
+start /wait /b "" "git\git-cmd.exe" git reset --hard origin/master
+timeout /t 3 /nobreak >nul
